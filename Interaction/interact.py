446e7437830a556c8ad86f713cc3867bbf614a48
abhit-rana <abhit20421@iiitd.ac.in>
2023-01-26 11:59:45 +0530

    Changes

--- Interaction/interact.py
+++ Interaction/interact.py
@@ -5,15 +5,18 @@ def give_bye_reply():
     return "Bye!"
 
 def extract_symptoms(info_dict):
+    print()
+
+def get_info_dict(user_input):
     
 
 def interact():
-    While(True):
+    while(True):
         user_input = input("Please enter the text:")
         info_dict = get_info_dict(user_input)
-        if(info_dict['user_intent']=='greetings'):
+        if(info_dict['intent']=='greetings'):
             print(give_greetings_reply())
-        elif(info_dict['user_intent']=='self_report'):
+        elif(info_dict['intent']=='self_report'):
             extract_symptoms(info_dict)
-        elif(info_dict['user_intent']=='good_bye'):
+        elif(info_dict['intent']=='good_bye'):
             print(give_bye_reply)
